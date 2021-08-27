0e65757b1c6d9c325b82de8fb6ccba7eea6529b1
Abdelrahman Madkour <a3madkour@gmail.com>
2021-08-27 15:41:25 +0200

    Actual first commit
    Setting up streamlit component with the code copied from the agraph
    streamlit component

---
+++ frontend/src/graph-component.tsx
@@ -0,0 +1,105 @@
+
+import React, { ReactNode, createRef, useEffect, useContext } from 'react';
+import { Network } from "vis-network/standalone/esm/vis-network"
+/* import { IGraph } from './util/graph'; */
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection
+} from "streamlit-component-lib";
+
+interface State {
+  network: any
+}
+interface Props {
+  /* graph: IGraph, */
+  args: any
+  /* width?: number,
+   * height?: number,
+   * updateParent?: () => void,
+   * manipulate: boolean
+   * letter: Letter */
+}
+
+class GraphComponent extends StreamlitComponentBase<State> {
+
+  public state = { network: {}, myRef: createRef<HTMLDivElement>() }
+
+
+  // create an array with edges
+
+  /* var data = {
+   *     nodes: props.graph.nodes,
+   *     edges: props.graph.edges,
+   * }; */
+
+
+  /* const graphs = ["first", "second", "third"] */
+
+  /* const handleChange = (e:any) =>{
+   *     return
+   * } */
+
+
+  public componentDidMount() {
+    const processGraph = (graph: any) => {
+
+      var nodes: Array<any> = []
+      var edges: Array<any> = []
+      var data = { nodes: nodes, edges: edges }
+
+      for (var i = 0; i < graph.nodes.length; i++) {
+        var node: any = Object.assign({}, graph.nodes[i]);
+        var svg =
+          '<svg xmlns="http://www.w3.org/2000/svg" width="75" height="75">' +
+          '<circle cx="37.5" cy="37.5" r="35" fill="#aeaeae" />' +
+          '<text x="50%" y="50%" text-anchor="middle" fill="white" font-size="x-large" font-family="Arial" dy=".3em">' + graph.nodes[i].abbrev + '</text>' +
+          'Sorry, your browser does not support inline SVG.' +
+          "</svg>";
+
+        var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svg);
+
+        node.shape = "circularImage";
+        node.image = url;
+        data.nodes.push(node)
+      }
+      data.edges = graph.edges
+
+      return data
+    }
+
+
+    const python_data = JSON.parse(this.props.args["data"])
+    const python_config = JSON.parse(this.props.args["config"])
+    const options = {
+      interaction: { hover: true },
+      /* width: python_config.width,
+* height: python_config.height, */
+      edges: {
+        arrows: { to: true }
+      },
+      manipulation: {
+        enabled: false
+      },
+
+    }
+    var data = processGraph(python_data)
+    var divElement = this.state.myRef.current
+    if (divElement) {
+      var network = new Network(divElement, data, options)
+    }
+
+
+    this.forceUpdate();
+  }
+  public render = (): ReactNode => {
+
+    return (
+      <div className="graph-container" ref={this.state.myRef} >
+      </div>
+    )
+
+  }
+}
+
+export default withStreamlitConnection(GraphComponent)
---
+++ frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import GraphComponent from "./graph-component"
+import AgraphComponent from "./AgraphComponent"
+import './App.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <GraphComponent/>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
